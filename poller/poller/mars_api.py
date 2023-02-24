4f6d6316fdf775c15d9b0946b5a2d4e7b26ef8b6
Tomi Kasurinen <tomi.kasurinen@gmail.com>
2023-02-24 13:22:31 +0200

    Change API endpoint.

--- poller/poller/mars_api.py
+++ poller/poller/mars_api.py
@@ -1,17 +1,9 @@
-import os
 from typing import Dict
 
 import requests
-from dotenv import load_dotenv
 
 
-load_dotenv()
-
-
-API_KEY = os.environ["API_KEY"]
-API_URL = (
-    f"https://api.nasa.gov/insight_weather/?api_key={API_KEY}&feedtype=json&ver=1.0"
-)
+API_URL = f"https://mars.nasa.gov/rss/api/?feed=weather&category=insight_temperature&feedtype=json&ver=1.0"
 
 
 def fetch_weather() -> Dict:
