886c88977b2a03a90706f7aba5137807290e5af8
Tomi Kasurinen <tomi.kasurinen@gmail.com>
2023-03-26 13:08:53 +0300

    Support s3 writes for poller.

--- poller/poller/cli.py
+++ poller/poller/cli.py
@@ -1,13 +1,12 @@
 import logging
 import os
 from datetime import datetime
-from pathlib import Path
 from poller.fs import save_as_json
 from poller.github import fetch_commit_stats
 
 
-EXPORT_DIR = "../data/raw/"
-
+# EXPORT_DIR = "../data/raw/"
+EXPORT_DIR = "s3://ew1-dev-awskerho-tomi-data/data/raw/"
 
 logging.basicConfig(
     format="%(asctime)s %(levelname)-8s %(message)s",
@@ -22,19 +21,17 @@ REPOS_TO_FETCH = [
 ]
 
 
-def get_export_path(user: str, repo: str) -> Path:
+def get_export_path(user: str, repo: str) -> str:
     now_iso = datetime.now()
     filename = "github_commit_stats.json"
-    return Path(
-        os.path.join(
-            EXPORT_DIR,
-            str(now_iso.year),
-            str(now_iso.month),
-            str(now_iso.day),
-            user,
-            repo,
-            filename,
-        )
+    return os.path.join(
+        EXPORT_DIR,
+        str(now_iso.year),
+        str(now_iso.month),
+        str(now_iso.day),
+        user,
+        repo,
+        filename,
     )
 
 
--- poller/poller/fs.py
+++ poller/poller/fs.py
@@ -1,10 +1,48 @@
 import json
+from functools import lru_cache
 from pathlib import Path
-from typing import Dict
+from typing import Dict, TYPE_CHECKING
+from urllib.parse import urlparse
+
+import boto3
+
+if TYPE_CHECKING:
+    from mypy_boto3_s3.service_resource import Object as S3Object
+    from mypy_boto3_s3 import S3ServiceResource
+else:
+    S3Object = object
+    S3ServiceResource = object
 
 
-def save_as_json(data: Dict, path: Path):
+def save_as_json(data: Dict, path: str):
     data_json = json.dumps(data)
-    path.parent.mkdir(parents=True, exist_ok=True)
-    with open(path, "w") as file:
+    if _is_s3_path(path):
+        _save_to_s3(data_json, path)
+    else:
+        _save_to_local_fs(data_json, path)
+
+
+@lru_cache(1)
+def _s3_res() -> S3ServiceResource:
+    return boto3.resource("s3")
+
+
+def _is_s3_path(path: str) -> bool:
+    return str(path).startswith("s3://")
+
+
+def _s3_object_from_path(path: str) -> S3Object:
+    url_components = urlparse(path)
+    return _s3_res().Object(url_components.netloc, url_components.path.lstrip("/"))
+
+
+def _save_to_local_fs(data_json: str, path: str):
+    path_obj = Path(path)
+    path_obj.parent.mkdir(parents=True, exist_ok=True)
+    with open(path_obj, "w") as file:
         file.write(data_json)
+
+
+def _save_to_s3(data_json: str, path: str):
+    s3_obj = _s3_object_from_path(path)
+    s3_obj.put(Body=data_json.encode("utf-8"))
